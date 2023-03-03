f5e24edf82b18692a89a78837130e25190ffbed7
AshwiniNakerakanti123 <126052000+AshwiniNakerakanti123@users.noreply.github.com>
2023-03-03 16:22:35 +0530

    doc: completed banana language

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var btnclick = document.querySelector("#btn");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var ashDiv = document.querySelector("#ash");
+//var ashDiv = document.querySelector("#ash");
+var outputDiv = document.querySelector("#output")
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 //var title = document.querySelector("#title");
 // ashDiv.innerHTML = "Ashwini";
 // console.log(txtInput);
@@ -10,7 +12,25 @@ var ashDiv = document.querySelector("#ash");
 //     console.log("input",txtInput.value);
 //    };
 // btnclick.addEventListener("click" ,clickhere);
-function clickhere(){
-   ashDiv.innerHTML = "hhbdgdjbx"+txtInput.value
+function getTranslationURL(text){
+   return serverURL + "?" +"text=" +text
+}
+function errorHandler(error){
+   console.log("error occured" , error);
+   alert("somthing wrong")
+}
+function clickHandler(){
+
+
+   // ashDiv.innerHTML = output + "?" + "text=" + text
+   var inputText = txtInput.value;
+
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => {
+   var translatedText = json.contents.translated;
+   outputDiv.innerText = translatedText;
+})
+.catch(errorHandler)
 };
-btnclick.addEventListener("click" ,clickhere);
+btnTranslate.addEventListener("click" ,clickHandler);
