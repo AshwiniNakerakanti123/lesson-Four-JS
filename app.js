8c25cc5b2170c82774dc0834acae87b881ac86ca
AshwiniNakerakanti123 <126052000+AshwiniNakerakanti123@users.noreply.github.com>
2023-03-03 12:58:33 +0530

    doc: new js app from banana lang

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var btnclick = document.querySelector("#btn");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var ashDiv = document.querySelector("#ash");
+//var title = document.querySelector("#title");
+// ashDiv.innerHTML = "Ashwini";
+// console.log(txtInput);
 
-   function clickhere(){
-    console.log("clicked");
-    console.log("input",txtInput.value);
-   };
+//    function clickhere(){
+//     console.log("clicked");
+//     console.log("input",txtInput.value);
+//    };
+// btnclick.addEventListener("click" ,clickhere);
+function clickhere(){
+   ashDiv.innerHTML = "hhbdgdjbx"+txtInput.value
+};
 btnclick.addEventListener("click" ,clickhere);
